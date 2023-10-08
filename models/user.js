92c7e3beccff44ec7c18f62333adc4e0f8df0ef3
Shivanshu-Tyagi <72153671+Shivanshu-Tyagi@users.noreply.github.com>
2023-10-08 16:04:03 +0530

    ui

--- models/user.js
+++ models/user.js
@@ -1,6 +1,11 @@
+
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  Mobile: {
+    type: Number,
+    required: true
+  },
   username: {
     type: String,
     required: true,
@@ -15,6 +20,16 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  referral_code: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  referral_point: {
+    type: Number,
+    default: 0
+  },
+  referral_link: String,
   uniqueID: {
     type: String,
     unique: true
--- routes/auth.js
+++ routes/auth.js
@@ -11,38 +11,106 @@ function generateUniqueID() {
   const randomString = Math.random().toString(36).substr(2, 5);
   return `${timestamp}${randomString}`;
 }
+const generateReferralCode = () => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let referralCode = '';
+
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    referralCode += characters.charAt(randomIndex);
+  }
+
+  console.log('Generated referral code:', referralCode);
+  return referralCode;
+};
 
 router.post('/register', async (req, res) => {
-  const { username, email, password } = req.body;
-  const uniqueID = generateUniqueID();
-
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({
-      username,
-      email,
-      password: hashedPassword,
-      uniqueID
-    });
-    await user.save();
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const user = await User.findOne({ username });
-    if (user && (await bcrypt.compare(password, user.password))) {
-      res.json({ message: 'Login successful', user });
+    const { fullname, username, email, password, referralcode } = req.body;
+  
+    let referredByUser = null;
+  
+    if (referralcode) {
+      try {
+        referredByUser = await User.findOne({ referral_code: referralcode });
+  
+        if (!referredByUser) {
+          return res.status(400).json({ error: 'Invalid referral code.' });
+        }
+  
+        // Increment points for the referrer and the referee
+        referredByUser.referral_point += 200;  // Referrer gets 200 points
+        await referredByUser.save();  // Await the save operation
+  
+        // Create the new user with a generated referral code and assign 100 points for using a referral code
+        const newUser = new User({
+          fullname: fullname,
+          username,
+          email,
+          password,
+          referral_code: generateReferralCode(),
+          referral_point: 100,  // User using a referral code gets 100 points
+        });
+  
+        await newUser.save();
+        res.json({ message: 'User registered successfully.' });
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
     } else {
-      res.status(401).json({ message: 'Invalid credentials' });
+      // If no referral code is provided, proceed with normal registration
+      const newUser = new User({
+        fullname: fullname,
+        username,
+        email,
+        password,
+        referral_code: generateReferralCode(),
+        referral_point: 0,
+      });
+  
+      try {
+        await newUser.save();
+           // Update the referral link for the new user
+           let referralLink = `http://127.0.0.1:5500/index.html?referralcode=${newUser.referral_code}`;
+           await User.updateOne({ _id: newUser._id }, { referral_link: referralLink });
+         
+           res.json({
+             message: 'User registered successfully.',
+             userInfo: {
+               name: newUser.fullname,
+               username: newUser.username,
+               email: newUser.email,
+               referralCode: newUser.referral_code,
+               referralPoints: newUser.referral_point,
+               referralLink: referralLink  // Include referralLink in the userInfo object
+             }
+        });
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
     }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  });
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email, password });
+
+    if (!user) {
+      return res.status(400).json({ error: 'Invalid email or password.' });
+    }
+
+    res.json({
+      message: `Welcome to this page, ${user.username}`,
+      userInfo: {
+        fullname: user.fullname,
+        username: user.username,
+        email: user.email,
+        referralCode: user.referral_code,
+        referralPoints: user.referral_point
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 router.post('/order', async (req, res) => {
@@ -81,33 +149,30 @@ router.post('/updatePrice', async (req, res) => {
   }
 });
 
-const fixedReferralCode = 'DHRAM04';
+router.get('/user/details/:username', async (req, res) => {
+  const { username } = req.params;
 
-router.post('/register', async (req, res) => {
-    const { username } = req.body;
+  try {
+    const user = await User.findOne({ username });
 
-    // Find the referrer by the fixed referral code
-    const referrer = await Refral.findOne({ username: fixedReferralCode });
-
-    if (referrer) {
-        // Create a new user
-        const newUser = new Refral({
-            username,
-            referralLink: `https://example.com/referral/${username}`,
-            walletBalance: 0
-        });
-
-        // Update referrer's wallet balance
-        referrer.walletBalance += 200;
-        await referrer.save();
-
-        // Save the new user
-        await newUser.save();
-
-        res.json({ message: 'User registered successfully.' });
-    } else {
-        res.status(400).json({ message: 'Referrer not found.' });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' });
     }
+
+    res.json({
+      message: 'User details retrieved successfully.',
+      userInfo: {
+        name: user.fullname,
+        username: user.username,
+        email: user.email,
+        referralCode: user.referral_code,
+        referralPoints: user.referral_point,
+        referralLink: user.referral_link 
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 
